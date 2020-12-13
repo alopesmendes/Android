df3c4b377b0110c479435a1e40c90089f4d91e2a
lopes <ailtonlopesmendes9@gmail.com>
2020-12-13 14:42:39 +0100

    readme change and test webservice

---
+++ TestWebService/app/src/main/java/com/example/testwebservice/RequestAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.testwebservice
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RequestAdapter(var requests : List<MainActivity.SearchResultEntry>) : RecyclerView.Adapter<RequestAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val textTitleView : TextView = itemView.findViewById(R.id.textTitle)
+
+        fun update(search: MainActivity.SearchResultEntry) {
+            textTitleView.text = "${search.id} -> ${search.title}"
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                        R.layout.activity_card,
+                        parent,
+                        false
+                )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.update(requests[position])
+    }
+
+    override fun getItemCount(): Int {
+        return requests.size
+    }
+}
