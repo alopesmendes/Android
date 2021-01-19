c506d066cf15d0ffb1f2b30b2a4ec5e87d20c78e
CHU Jonathan <46070603+Jonakyll@users.noreply.github.com>
2021-01-19 15:45:27 +0100

    sqlite
    mise à jour des fonctionnalités de la bdd sqlite, insertion d'une config via le service ConfigurationPusher, suppression d'une config, select de toutes les configs

--- Test_Confroid/app/src/main/java/fr/umlv/test_confroid/DatabaseHandler.kt
+++ Test_Confroid/app/src/main/java/fr/umlv/test_confroid/DatabaseHandler.kt
@@ -2,10 +2,14 @@ package fr.umlv.test_confroid
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import androidx.core.content.contentValuesOf
-import java.net.IDN
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import java.sql.Date
+import java.time.LocalDate
 
 class DatabaseHandler(
     context: Context?,
@@ -17,13 +21,17 @@ class DatabaseHandler(
     companion object {
         val DATABASE_VERSION: Int = 1
         val DATABASE_NAME: String = "configDB"
+
         val TABLE: String = "configs"
         val KEY_ID: String = "id"
+        val KEY_VERSION: String = "version"
         val KEY_CONTENT: String = "content"
+        val KEY_TAG: String = "tag"
+        val KEY_DATE: String = "date"
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL("CREATE TABLE $TABLE($KEY_ID INTEGER PRIMARY KEY, $KEY_CONTENT TEXT)")
+        db?.execSQL("CREATE TABLE $TABLE($KEY_ID INTEGER PRIMARY KEY AUTOINCREMENT, $KEY_VERSION INTEGER, $KEY_CONTENT TEXT, $KEY_TAG TEXT, $KEY_DATE DATE)")
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
@@ -32,65 +40,5 @@ class DatabaseHandler(
 
         // Create tables again
         onCreate(db);
-    }
-
-    fun addConfig(config: Config) {
-        val db = this.writableDatabase
-        val values = ContentValues()
-        values.put(KEY_CONTENT, config.content)
-
-        db.insert(TABLE, null, values)
-        db.close()
-    }
-
-    fun getContact(id: Int): Config {
-        val db = this.readableDatabase
-        val cursor = db.query(
-            TABLE, arrayOf(KEY_CONTENT), KEY_ID + "=?",
-            arrayOf(id.toString()), null, null, null, null
-        )
-
-        cursor?.moveToFirst()
-
-        val config = Config(cursor.getInt(0), cursor.getString(1))
-        return config
-    }
-
-    fun getAllConfigs(): List<Config> {
-        val configList = ArrayList<Config>()
-        val selectQuery = "SELECT * FROM " + TABLE
-        val db = this.writableDatabase
-        val cursor = db.rawQuery(selectQuery, null)
-
-        if (cursor.moveToFirst()) {
-            do {
-                val config = Config(cursor.getInt(0), cursor.getString(1))
-                configList.add(config)
-            } while (cursor.moveToNext())
-        }
-        return configList
-    }
-
-    fun updateConfig(config: Config): Int {
-        val db = this.writableDatabase
-
-        val values = ContentValues()
-        values.put(KEY_CONTENT, config.content)
-
-        return db.update(TABLE, values, KEY_ID + "=?", arrayOf(config.id.toString()))
-    }
-
-    fun deleteConfig(config: Config) {
-        val db = this.writableDatabase
-        db.delete(TABLE, KEY_ID + "=?", arrayOf(config.id.toString()))
-        db.close()
-    }
-
-    fun getConfigsCount(): Int {
-        val countQuery = "SELECT * FROM " + TABLE
-        val db = this.readableDatabase
-        val cursor = db.rawQuery(countQuery, null)
-
-        return cursor.count
     }
 }
