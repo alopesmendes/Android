ea37246380da00651b0624bbadbd45e94faea5ab
alopesmendes <ailtonlopesmendes9@gmail.com>
2021-02-28 22:21:34 +0100

    Menu start

---
+++ Confroid/app/src/main/java/fr/uge/confroid/web/AppFragment.kt
@@ -0,0 +1,9 @@
+package fr.uge.confroid.web
+
+import androidx.fragment.app.Fragment
+import fr.uge.confroid.R
+
+class AppFragment : Fragment(R.layout.fragment_app) {
+    // TODO: Rename and change types of parameters
+
+}
---
+++ Confroid/app/src/main/java/fr/uge/confroid/web/FilesFragment.kt
@@ -0,0 +1,12 @@
+package fr.uge.confroid.web
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import fr.uge.confroid.R
+
+class FilesFragment : Fragment(R.layout.fragment_files) {
+
+}
