0053fbbb7b9df6b8ebdc75135fce3c4ec83872f8
FLambertD <f.lambert--delavaquerie@orange.fr>
2021-03-01 15:41:00 +0100

    display config description

--- Test_Confroid/app/src/main/java/fr/umlv/test_confroid/ConfigActivity.kt
+++ Test_Confroid/app/src/main/java/fr/umlv/test_confroid/ConfigActivity.kt
@@ -2,10 +2,26 @@ package fr.umlv.test_confroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_display_config.*
 
 class ConfigActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_config)
+        setContentView(R.layout.activity_display_config)
+
+        val intent = intent
+        if (intent != null) {
+            var config: Config
+            if (intent.hasExtra("config")) {
+                config = intent.extras?.get("config") as Config
+                AppNameConfig.text = config.app
+                textVersionConfig.text = config.version.toString()
+                textTagConfig.text = config.tag
+                textContent.text = config.content
+            }
+        }
     }
 }
