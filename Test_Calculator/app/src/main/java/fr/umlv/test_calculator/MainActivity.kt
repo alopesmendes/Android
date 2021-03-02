0cecf36f748b07f8d903776b7e6b067e749218de
CHU Jonathan <46070603+Jonakyll@users.noreply.github.com>
2021-03-02 16:48:55 +0100

    save token
    le token est sauvegardé dans le fichier de préférences de l'app,
    pour supprimer le fichier de préférences, il faut désinstaller l'app à la main

--- Test_Calculator/app/src/main/java/fr/umlv/test_calculator/MainActivity.kt
+++ Test_Calculator/app/src/main/java/fr/umlv/test_calculator/MainActivity.kt
@@ -1,6 +1,8 @@
 package fr.umlv.test_calculator
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import fr.umlv.test_calculator.utils.ConfroidUtils
@@ -8,11 +10,23 @@ import kotlinx.android.synthetic.main.activity_all_versions.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val prefs = getSharedPreferences("prefs", Context.MODE_PRIVATE)
+
+        if (intent != null) {
+            val token = intent.extras?.getLong("token", 0L)
+            if (token != null) {
+                prefs.edit().putLong("token", token).apply()
+            }
+        }
+        CalculatorConfig.TOKEN = prefs.getLong("token", 0L)
         token_tv.text = "TOKEN: ${CalculatorConfig.TOKEN}"
+
+        /////////////////////////////
 
         send_config_button.setOnClickListener {
             if (CalculatorConfig.TOKEN == 0L) {
