501c48b228a3020f2077c8305403171dcb353d39
alopesmendes <ailtonlopesmendes9@gmail.com>
2021-03-18 21:02:59 +0100

    viewpager2 with fragments

---
+++ Confroid/app/src/main/java/fr/uge/confroid/settings/help/fragments/HelpMenuFragment.kt
@@ -0,0 +1,14 @@
+package fr.uge.confroid.settings.help.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import fr.uge.confroid.R
+
+
+class HelpMenuFragment : Fragment(R.layout.fragment_help_menu) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+    }
+}
