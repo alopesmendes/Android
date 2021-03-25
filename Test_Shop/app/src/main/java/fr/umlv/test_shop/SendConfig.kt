297ba5a6f58867cac5fd808b2d4254f5381a0606
CHU Jonathan <46070603+Jonakyll@users.noreply.github.com>
2021-03-25 15:01:26 +0100

    Update SendConfig.kt

--- Test_Shop/app/src/main/java/fr/umlv/test_shop/SendConfig.kt
+++ Test_Shop/app/src/main/java/fr/umlv/test_shop/SendConfig.kt
@@ -20,21 +20,37 @@ class SendConfig : AppCompatActivity() {
         val prefs = ShoppingPreferences(mutableMapOf())
 
         add_address_button.setOnClickListener {
-            val address = ShippingAddress(
-                name_et.text.toString(),
-                street_et.text.toString(),
-                city_et.text.toString(),
-                country_et.text.toString()
-            )
-            val billingDetail = BillingDetail(
-                card_holder_et.text.toString(),
-                card_number_et.text.toString(),
-                month_et.text.toString().toInt(),
-                year_et.text.toString().toInt(),
-                cryptogram_et.text.toString().toInt()
-            )
-            val shopInfo = ShoppingInfo(address, billingDetail, favorite_cb.isChecked)
-            prefs.shoppingInfos[info_title_et.text.toString()] = shopInfo
+            val name = name_et.text.toString()
+            val street = street_et.text.toString()
+            val city = city_et.text.toString()
+            val country = country_et.text.toString()
+            val card_holder = card_holder_et.text.toString()
+            val card_number = card_number_et.text.toString()
+            val month = month_et.text.toString()
+            val year = year_et.text.toString()
+            val cryptogram = cryptogram_et.text.toString()
+            val label = info_title_et.text.toString()
+
+            if (name.isBlank() || street.isBlank() || city.isBlank() || country.isBlank() || card_holder.isBlank() || card_number.isBlank() || month.isBlank() || year.isBlank() || cryptogram.isBlank() || label.isBlank()) {
+                Toast.makeText(this, "please fill the fields", Toast.LENGTH_SHORT).show()
+            } else {
+                val address = ShippingAddress(
+                    name,
+                    street,
+                    city,
+                    country
+                )
+
+                val billingDetail = BillingDetail(
+                    card_holder,
+                    card_number,
+                    month.toInt(),
+                    year.toInt(),
+                    cryptogram.toInt()
+                )
+                val shopInfo = ShoppingInfo(address, billingDetail, favorite_cb.isChecked)
+                prefs.shoppingInfos[label] = shopInfo
+            }
         }
 
         show_config_button.setOnClickListener {
